30cb853cd3dd4818865c183a8cda911b0d757adc
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2024-10-01 21:45:38 +0700

    Getting cooked
    Help

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
 import HomePage from "./pages/Homepage";
 import AuthPage from "./pages/AuthPage";
 import CodeEditorPage from "./pages/CodeEditorPage";
+import ProfilePage from "./pages/ProfilePage";
 import styled from "styled-components";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const AppContainer = styled.div`
   width: 100%;
   min-height: 100vh;
   display: flex;
-  flex-direction: column;
-  padding-top: 60px;
+`;
+
+const ContentContainer = styled.div`
+  flex-grow: 1;
+  padding: 20px;
 `;
 
 function App() {
   return (
     <Router>
-      <Header />
       <AppContainer>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/editor" element={<CodeEditorPage />} />
-        </Routes>
+        <Sidebar />
+        <ContentContainer>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth" element={<AuthPage />} />
+            <Route path="/editor" element={<CodeEditorPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </ContentContainer>
       </AppContainer>
     </Router>
   );
 }
+
 export default App;
