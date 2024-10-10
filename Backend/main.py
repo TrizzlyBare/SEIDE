8e35ee8347bb9c06bb2e9ea43f65d006c71924c5
TrizzlyBare <149318921+TrizzlyBare@users.noreply.github.com>
2024-10-11 01:15:07 +0700

    Main is dead

--- Backend/main.py
+++ Backend/main.py
@@ -1,51 +1,29 @@
 from ast import List
-from fastapi import FastAPI, Request, Form, Depends, HTTPException
+from fastapi import FastAPI, Request, Form, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from typing import Optional, List
-import time
-import logging
 from passlib.context import CryptContext
-from typing import Dict, Any, List, Optional
 from fastapi.middleware.cors import CORSMiddleware
-
-# from . import schemas, crud
+import logging
+import time
+from typing import List
 
 from database import SessionLocal, User
-from dashboard import (Subject, Topic, Question, Answer, TestCase, DashboardSessionLocal, SubjectBase, TopicBase,
-QuestionBase, AnswerBase, TestCaseBase, TopicCreate, QuestionCreate, AnswerCreate, TestCaseCreate,
-SubjectCreate)
-from editor import EditorSessionLocal, Create_Code_Data, CodeData
-from user_profile import Profile, ProfileSessionLocal, ProfileCreate
-from schemas import SubjectBase, SubjectCreate, Subject
+from dashboard import (Subject, Topic, Question, Answer, TestCase, 
+                       DashboardSessionLocal, SubjectCreate, 
+                       TopicCreate, QuestionCreate, 
+                       AnswerCreate, TestCaseCreate, AnswerBase, TestCaseBase, QuestionBase, TopicBase, SubjectBase)    
+from editor import EditorSessionLocal
+from user_profile import ProfileSessionLocal
+from schemas import Subject, SubjectCreate
 
+# Initialize the FastAPI application
 app = FastAPI()
 
-origins = [
-    "http://localhost:5174",
-]
-
-# Set up templates directory
-templates = Jinja2Templates(directory="templates")
-
-# Set up password hashing
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-# Middleware to log request method and URL
-@app.middleware("http")
-async def log_requests(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    print(
-        f"Request: {request.method} {request.url} completed in {process_time:.4f} seconds"
-    )
-    return response
-
-
+# CORS configuration
+origins = ["http://localhost:5174"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -54,6 +32,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Set up templates directory
+templates = Jinja2Templates(directory="templates")
+
+# Set up password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Middleware to log request method and URL
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    logging.info(f"Request: {request.method} {request.url} completed in {process_time:.4f} seconds")
+    return response
 
 # Dependency to get the database session
 def get_db():
@@ -62,7 +54,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
 
 def get_dashboard_db():
     db = DashboardSessionLocal()
@@ -78,86 +69,60 @@ def get_editor_db():
     finally:
         db.close()
 
-
-def get_Profiles_db():
+def get_profiles_db():
     db = ProfileSessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-
 # Serve the login.html file
 @app.get("/login", response_class=HTMLResponse)
 async def get_login_form(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
-
 
 # Pydantic model for login data
 class LoginData(BaseModel):
     username: str
     password: str
 
-
 # Handle login form submission
 @app.post("/login")
-async def login(
-    username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)
-):
-    try:
-        user = db.query(User).filter(User.username == username).first()
-        if user and pwd_context.verify(password, user.hashed_password):
-            return {"message": "Login successful"}
-        raise HTTPException(status_code=400, detail="Invalid credentials")
-    except Exception as e:
-        logging.error(f"Error during login: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
+async def login(username: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == username).first()
+    if user and pwd_context.verify(password, user.hashed_password):
+        return {"message": "Login successful"}
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid credentials")
 
 @app.post("/register")
-async def register(
-    username: str = Form(...),
-    email: str = Form(...),
-    password: str = Form(...),
-    db: Session = Depends(get_db),
-):
-    try:
-        logging.info(f"Registering user: {username}, {email}")
-        user = db.query(User).filter(User.username == username).first()
-        if user:
-            logging.warning(f"Username already registered: {username}")
-            raise HTTPException(status_code=400, detail="Username already registered")
-        hashed_password = pwd_context.hash(password)
-        new_user = User(username=username, email=email, hashed_password=hashed_password)
-        db.add(new_user)
-        db.commit()
-        logging.info(f"User created successfully: {username}")
-        return {"message": "User created successfully"}
-    except Exception as e:
-        logging.error(f"Error during registration: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+async def register(username: str = Form(...), email: str = Form(...), password: str = Form(...), db: Session = Depends(get_db)):
+    logging.info(f"Registering user: {username}, {email}")
+    user = db.query(User).filter(User.username == username).first()
+    if user:
+        logging.warning(f"Username already registered: {username}")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already registered")
+    
+    hashed_password = pwd_context.hash(password)
+    new_user = User(username=username, email=email, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    logging.info(f"User created successfully: {username}")
+    return {"message": "User created successfully"}
 
+# Dashboard routes
 @app.get("/dashboard")
 async def get_subjects(db: Session = Depends(get_dashboard_db)):
     subjects = db.query(Subject).all()
     return subjects
 
 @app.post("/subjects/", response_model=Subject)
-def create_subject(subject: SubjectCreate, db: Session = Depends(get_dashboard_db)):
-    try:
-        logging.info(f"Received payload: {subject.dict()}") # Log the received payload
-        db_subject = Subject(**subject.dict())
-        db.add(db_subject)
-        db.commit()
-        db.refresh(db_subject)
-        logging.info(f"Subject created: {db_subject}")
-        return db_subject
-    except ValidationError as e: # type: ignore
-        logging.error(f"Validation error: {e.errors()}") # Log validation errors
-        raise HTTPException(status_code=422, detail=e.errors())
-    except Exception as e:
-        logging.error(f"Error creating subject: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+async def create_subject(subject: SubjectCreate, db: Session = Depends(get_db)):
+    db_subject = Subject(**subject.dict())
+    db.add(db_subject)
+    db.commit()
+    db.refresh(db_subject)
+    return db_subject
 
 @app.get("/subjects/", response_model=List[Subject])
 def read_subjects(skip: int = 0, limit: int = 10, db: Session = Depends(get_dashboard_db)):
@@ -168,19 +133,18 @@ def read_subjects(skip: int = 0, limit: int = 10, db: Session = Depends(get_dash
 def read_subject(subject_id: int, db: Session = Depends(get_dashboard_db)):
     subject = db.query(Subject).filter(Subject.subject_id == subject_id).first()
     if subject is None:
-        raise HTTPException(status_code=404, detail="Subject not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Subject not found")
     return subject
 
-@app.put("/subjects/{subject_id}", response_model=SubjectBase)
+@app.put("/subjects/{subject_id}", response_model=Subject)
 def update_subject(subject_id: int, subject: SubjectCreate, db: Session = Depends(get_dashboard_db)):
     db_subject = db.query(Subject).filter(Subject.subject_id == subject_id).first()
     if db_subject is None:
-        raise HTTPException(status_code=404, detail="Subject not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Subject not found")
     
     for key, value in subject.dict().items():
         setattr(db_subject, key, value)
-    
-    db.add(db_subject)
+
     db.commit()
     db.refresh(db_subject)
     return db_subject
@@ -189,7 +153,7 @@ def update_subject(subject_id: int, subject: SubjectCreate, db: Session = Depend
 def delete_subject(subject_id: int, db: Session = Depends(get_dashboard_db)):
     db_subject = db.query(Subject).filter(Subject.subject_id == subject_id).first()
     if db_subject is None:
-        raise HTTPException(status_code=404, detail="Subject not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Subject not found")
     db.delete(db_subject)
     db.commit()
     return {"detail": "Subject deleted"}
@@ -212,19 +176,18 @@ def read_topics(skip: int = 0, limit: int = 10, db: Session = Depends(get_dashbo
 def read_topic(topic_id: int, db: Session = Depends(get_dashboard_db)):
     topic = db.query(Topic).filter(Topic.topic_id == topic_id).first()
     if topic is None:
-        raise HTTPException(status_code=404, detail="Topic not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Topic not found")
     return topic
 
 @app.put("/topics/{topic_id}", response_model=TopicBase)
 def update_topic(topic_id: int, topic: TopicCreate, db: Session = Depends(get_dashboard_db)):
     db_topic = db.query(Topic).filter(Topic.topic_id == topic_id).first()
     if db_topic is None:
-        raise HTTPException(status_code=404, detail="Topic not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Topic not found")
     
     for key, value in topic.dict().items():
         setattr(db_topic, key, value)
     
-    db.add(db_topic)
     db.commit()
     db.refresh(db_topic)
     return db_topic
@@ -233,7 +196,7 @@ def update_topic(topic_id: int, topic: TopicCreate, db: Session = Depends(get_da
 def delete_topic(topic_id: int, db: Session = Depends(get_dashboard_db)):
     db_topic = db.query(Topic).filter(Topic.topic_id == topic_id).first()
     if db_topic is None:
-        raise HTTPException(status_code=404, detail="Topic not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Topic not found")
     db.delete(db_topic)
     db.commit()
     return {"detail": "Topic deleted"}
@@ -256,19 +219,18 @@ def read_questions(skip: int = 0, limit: int = 10, db: Session = Depends(get_das
 def read_question(question_id: int, db: Session = Depends(get_dashboard_db)):
     question = db.query(Question).filter(Question.question_id == question_id).first()
     if question is None:
-        raise HTTPException(status_code=404, detail="Question not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Question not found")
     return question
 
 @app.put("/questions/{question_id}", response_model=QuestionBase)
 def update_question(question_id: int, question: QuestionCreate, db: Session = Depends(get_dashboard_db)):
     db_question = db.query(Question).filter(Question.question_id == question_id).first()
     if db_question is None:
-        raise HTTPException(status_code=404, detail="Question not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Question not found")
     
     for key, value in question.dict().items():
         setattr(db_question, key, value)
     
-    db.add(db_question)
     db.commit()
     db.refresh(db_question)
     return db_question
@@ -277,7 +239,7 @@ def update_question(question_id: int, question: QuestionCreate, db: Session = De
 def delete_question(question_id: int, db: Session = Depends(get_dashboard_db)):
     db_question = db.query(Question).filter(Question.question_id == question_id).first()
     if db_question is None:
-        raise HTTPException(status_code=404, detail="Question not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Question not found")
     db.delete(db_question)
     db.commit()
     return {"detail": "Question deleted"}
@@ -300,19 +262,18 @@ def read_answers(skip: int = 0, limit: int = 10, db: Session = Depends(get_dashb
 def read_answer(answer_id: int, db: Session = Depends(get_dashboard_db)):
     answer = db.query(Answer).filter(Answer.answer_id == answer_id).first()
     if answer is None:
-        raise HTTPException(status_code=404, detail="Answer not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Answer not found")
     return answer
 
 @app.put("/answers/{answer_id}", response_model=AnswerBase)
 def update_answer(answer_id: int, answer: AnswerCreate, db: Session = Depends(get_dashboard_db)):
     db_answer = db.query(Answer).filter(Answer.answer_id == answer_id).first()
     if db_answer is None:
-        raise HTTPException(status_code=404, detail="Answer not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Answer not found")
     
     for key, value in answer.dict().items():
         setattr(db_answer, key, value)
     
-    db.add(db_answer)
     db.commit()
     db.refresh(db_answer)
     return db_answer
@@ -321,13 +282,13 @@ def update_answer(answer_id: int, answer: AnswerCreate, db: Session = Depends(ge
 def delete_answer(answer_id: int, db: Session = Depends(get_dashboard_db)):
     db_answer = db.query(Answer).filter(Answer.answer_id == answer_id).first()
     if db_answer is None:
-        raise HTTPException(status_code=404, detail="Answer not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Answer not found")
     db.delete(db_answer)
     db.commit()
     return {"detail": "Answer deleted"}
 
 # CRUD for Test Cases
-@app.post("/questions/{question_id}/test_cases/", response_model=TestCaseBase)
+@app.post("/questions/{question_id}/testcases/", response_model=TestCaseBase)
 def create_test_case(question_id: int, test_case: TestCaseCreate, db: Session = Depends(get_dashboard_db)):
     db_test_case = TestCase(**test_case.dict(), question_id=question_id)
     db.add(db_test_case)
@@ -335,40 +296,39 @@ def create_test_case(question_id: int, test_case: TestCaseCreate, db: Session = 
     db.refresh(db_test_case)
     return db_test_case
 
-@app.get("/test_cases/", response_model=List[TestCaseBase])
+@app.get("/testcases/", response_model=List[TestCaseBase])
 def read_test_cases(skip: int = 0, limit: int = 10, db: Session = Depends(get_dashboard_db)):
     test_cases = db.query(TestCase).offset(skip).limit(limit).all()
     return test_cases
 
-@app.get("/test_cases/{test_case_id}", response_model=TestCaseBase)
+@app.get("/testcases/{test_case_id}", response_model=TestCaseBase)
 def read_test_case(test_case_id: int, db: Session = Depends(get_dashboard_db)):
     test_case = db.query(TestCase).filter(TestCase.test_case_id == test_case_id).first()
     if test_case is None:
-        raise HTTPException(status_code=404, detail="Test Case not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Test case not found")
     return test_case
 
-@app.put("/test_cases/{test_case_id}", response_model=TestCaseBase)
+@app.put("/testcases/{test_case_id}", response_model=TestCaseBase)
 def update_test_case(test_case_id: int, test_case: TestCaseCreate, db: Session = Depends(get_dashboard_db)):
     db_test_case = db.query(TestCase).filter(TestCase.test_case_id == test_case_id).first()
     if db_test_case is None:
-        raise HTTPException(status_code=404, detail="Test Case not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Test case not found")
     
     for key, value in test_case.dict().items():
         setattr(db_test_case, key, value)
     
-    db.add(db_test_case)
     db.commit()
     db.refresh(db_test_case)
     return db_test_case
 
-@app.delete("/test_cases/{test_case_id}")
+@app.delete("/testcases/{test_case_id}")
 def delete_test_case(test_case_id: int, db: Session = Depends(get_dashboard_db)):
     db_test_case = db.query(TestCase).filter(TestCase.test_case_id == test_case_id).first()
     if db_test_case is None:
-        raise HTTPException(status_code=404, detail="Test Case not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Test case not found")
     db.delete(db_test_case)
     db.commit()
-    return {"detail": "Test Case deleted"}
+    return {"detail": "Test case deleted"}
 
 @app.get("/logout")
 async def logout():
